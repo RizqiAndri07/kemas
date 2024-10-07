6e3b4c1065a0ec61f40e1de6695904e151fc513b
Kinggu <rizqiandri12la@gmail.com>
2024-10-07 14:53:43 +0700

    Initial commit

---
+++ coba.java
@@ -0,0 +1,15 @@
+class coba{
+    public static void main(String[] args) {
+        
+        System.out.println();
+    }
+    long pendapatanValueAkhir = 0;
+    public long pendapatanValue(){
+        return pendapatanValueAkhir;
+    }
+
+    public void hitungpendapatanValue(){
+        pendapatanValueAkhir = 10;
+        System.out.println(pendapatanValueAkhir);
+    }
+}
